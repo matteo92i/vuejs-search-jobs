eaea0c3f85f1f21d05bbcac29a362b811fbece49
Matteo92i <matteo.iovane@gmail.com>
2021-07-06 11:09:44 +0200

    inizio

---
+++ js/script.js
@@ -0,0 +1,64 @@
+new Vue({
+    el: '#app',
+    data: {
+        jobs: [
+            {
+              id: 1,
+              company: 'Roma',
+              position: 'calciatore',
+              description: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure veniam similique adipisci! Perferendis odio sapiente libero quam deleniti quidem consequuntur adipisci minima non iusto, sunt optio hic. Corporis, laboriosam perferendis?',
+              created_at: '5/22/2021',
+              logo: 'logo.jpg',
+              city: 'Roma',
+              contract: 'Full Time',
+            },
+            {
+                id: 2,
+                company: 'Perferendis',
+                position: 'Developer',
+                description: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure veniam similique adipisci! Perferendis odio sapiente libero quam deleniti quidem consequuntur adipisci minima non iusto, sunt optio hic. Corporis, laboriosam perferendis?',
+                created_at: '5/22/2021',
+                logo: 'logo.jpg',
+                city: 'Palermo',
+                contract: 'Part Time',
+            },
+            {      
+                id: 3,
+                company: 'ospedale',
+                position: 'Medico',
+                description: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure veniam similique adipisci! Perferendis odio sapiente libero quam deleniti quidem consequuntur adipisci minima non iusto, sunt optio hic. Corporis, laboriosam perferendis?',
+                created_at: '5/22/2021',
+                logo: 'logo.jpg',
+                city: 'Milano',
+                contract: 'indeterminato',
+            },
+            {
+                id: 4,
+                company: 'Ospedale',
+                position: 'Infermiere',
+                description: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure veniam similique adipisci! Perferendis odio sapiente libero quam deleniti quidem consequuntur adipisci minima non iusto, sunt optio hic. Corporis, laboriosam perferendis?',
+                created_at: '5/22/2021',
+                logo: 'logo.jpg',
+                city: 'Torino',
+                contract: 'determinato',
+            },
+            {
+                id: 5,
+                company: 'Ospedale',
+                position: 'Tecnico',
+                description: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure veniam similique adipisci! Perferendis odio sapiente libero quam deleniti quidem consequuntur adipisci minima non iusto, sunt optio hic. Corporis, laboriosam perferendis?',
+                created_at: '5/22/2021',
+                logo: 'logo.jpg',
+                city: 'Bologna',
+                contract: 'Full Time',
+            },
+        ],
+    }
+        
+}
+);
+        
+
+
+
+
